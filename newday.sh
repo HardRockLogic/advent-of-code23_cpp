e2b49fdc9ae4790854c9e1533893056ebfae1da8
HardRockLogic <neiton5001@gmail.com>
2024-01-11 17:41:00 +0200

    updated script to create default makefile, added check for existing file

--- newday.sh
+++ newday.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
+if [ -d "$1" ]; then
+  echo "directory already exists"
+  exit 1;
+fi
+
 if [ $# -eq 1 ]; then
-  mkdir -p $1
-  touch $1/main.cpp
+  mkdir $1
+  touch $1/main.cpp $1/Makefile
+  
+  cat <<'EOF' > $1/Makefile
+  CXX = g++
+  CXXFLAGS = -std=c++20 -Wall -Wextra
+
+  SRC = main.cpp
+  EXE = main
+
+  all: $(EXE)
+
+  $(EXE): $(SRC)
+    $(CXX) $(CXXFLAGS) -o $@ $^
+
+  clean:
+    @rm -f $(EXE)
+    
+EOF
+
 else 
   echo "invalid or no arguments supplied"
 fi
+
+echo "$1 created successfully"
