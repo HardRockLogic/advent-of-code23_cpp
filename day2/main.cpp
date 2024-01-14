5ff04c3fefc749d01eb4ac996272ca53804239e0
HardRockLogic <neiton5001@gmail.com>
2024-01-14 23:14:30 +0200

    day2 part2 done

--- day2/main.cpp
+++ day2/main.cpp
@@ -1,9 +1,12 @@
 #include <fstream>
+#include <functional>
 #include <iostream>
+#include <numeric>
 #include <regex>
 #include <string>
+#include <unordered_map>
 
-int main() {
+void partOne() {
   std::ifstream file("input.txt");
 
   std::regex pattern(R"((\d+)\s*([a-zA-Z]+))");
@@ -46,4 +49,57 @@ int main() {
   }
 
   std::cout << accumulator << "\n";
+}
+
+void partTwo() {
+  std::ifstream file("input.txt");
+
+  std::regex pattern(R"((\d+)\s*([a-zA-Z]+))");
+
+  unsigned accumulator = 0;
+  // unsigned counter = 0;
+
+  if (file.is_open()) {
+    std::string line;
+
+    while (std::getline(file, line)) {
+      // ++counter;
+      std::unordered_map<std::string, unsigned> bag;
+      // bool adding_condition = true;
+
+      auto colors_begin =
+          std::sregex_iterator(line.begin(), line.end(), pattern);
+      auto colors_end = std::sregex_iterator();
+
+      for (auto iter = colors_begin; iter != colors_end; ++iter) {
+        std::smatch match = *iter;
+        unsigned number_cubes = std::stoi(match[1].str());
+        std::string color = match[2].str();
+
+        auto map_entry = bag.find(color);
+        if (map_entry == bag.end()) {
+          bag[color] = number_cubes;
+        } else {
+          if (number_cubes > map_entry->second) {
+            map_entry->second = number_cubes;
+          }
+        }
+      }
+      accumulator += std::accumulate(
+          bag.begin(), bag.end(), 1u,
+          [](unsigned acc, const std::pair<std::string, unsigned> &pair) {
+            return acc * pair.second;
+          });
+    }
+  } else {
+    std::cerr << "error opening file\n";
+  }
+
+  std::cout << accumulator << "\n";
+}
+
+int main() {
+  partOne();
+  std::cout << "------part two-------\n";
+  partTwo();
 }
