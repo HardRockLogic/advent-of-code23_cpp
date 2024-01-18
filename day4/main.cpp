84a80ba921311024655e249b0983d8e0a9415ca4
HardRockLogic <neiton5001@gmail.com>
2024-01-18 19:13:17 +0200

    day4 part2 complete

--- day4/main.cpp
+++ day4/main.cpp
@@ -5,11 +5,13 @@
 #include <fmt/ranges.h>
 #include <fstream>
 #include <iostream>
+#include <numeric>
 #include <ranges>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
-class ScratchCard {
+class ScratchCardP1 {
 private:
   std::vector<unsigned> left_part_;
   std::vector<unsigned> right_part_;
@@ -57,10 +59,82 @@ public:
   }
 };
 
+class ScratchCardP2 {
+private:
+  std::vector<unsigned> left_part_;
+  std::vector<unsigned> right_part_;
+  std::unordered_map<unsigned, unsigned> cards_data;
+  unsigned card_id = 0;
+  unsigned short count_state_ = 1;
+  unsigned acc_ = 0;
+
+  [[nodiscard]] std::vector<unsigned>
+  produce_duplicats(std::vector<unsigned> input) {
+    std::vector<unsigned> output;
+
+    for (unsigned const card : input) {
+      unsigned res = cards_data[card];
+
+      for (unsigned i = card + 1; i <= card + res; ++i) {
+        output.push_back(i);
+      }
+    }
+
+    return output;
+  }
+
+public:
+  void push_number(unsigned number) {
+    if (count_state_ <= 10) {
+      left_part_.push_back(number);
+    } else {
+      right_part_.push_back(number);
+    }
+
+    ++count_state_;
+  }
+
+  void check_numbers() {
+    unsigned multiplier = 0;
+    ++card_id;
+
+    for (unsigned const your_number : left_part_) {
+      auto result =
+          std::find(right_part_.begin(), right_part_.end(), your_number);
+      if (result != right_part_.end()) {
+        ++multiplier;
+      }
+    }
+
+    cards_data[card_id] = multiplier;
+
+    count_state_ = 1;
+    left_part_.clear();
+    right_part_.clear();
+  }
+
+  void count_cards() {
+    std::vector<unsigned> cards_iteration(cards_data.size());
+    std::iota(cards_iteration.begin(), cards_iteration.end(), 1);
+
+    while (cards_iteration.size()) {
+      acc_ += cards_iteration.size();
+      cards_iteration = this->produce_duplicats(cards_iteration);
+    }
+  }
+
+  unsigned accumulated() const { return acc_; }
+
+  void show_res_debug() {
+    fmt::print("{} \n---------\n", left_part_);
+    fmt::print("{} \n", right_part_);
+  }
+};
+
 int main() {
   // std::string s = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
   std::ifstream file("input.txt");
-  ScratchCard card;
+  ScratchCardP2 card;
 
   if (file.is_open()) {
     std::string line;
@@ -71,8 +145,12 @@ int main() {
         return (x.size() == 0 || x[0] == '|') ? false : true;
       };
 
-      auto result = line | std::views::split(' ') |
-                    std::views::filter(is_empty) | std::views::drop(2);
+      // clang-format off
+      auto result = line 
+        | std::views::split(' ') 
+        | std::views::filter(is_empty) 
+        | std::views::drop(2);
+      // clang-format on
 
       // fmt::print("{} \n", fmt::join(result, " "));
 
@@ -98,32 +176,9 @@ int main() {
     std::cerr << "error opening file\n";
   }
 
+  // this method is only necessary for part 2
+  card.count_cards();
+
+  // part2 asnwer: 6874754
   fmt::print("Part one: {}\n", card.accumulated());
-
-  // auto const is_empty = [](auto x) {
-  //   return (x.size() == 0 || x[0] == '|') ? false : true;
-  // };
-  //
-  // auto result = s | std::views::split(' ') | std::views::drop(2) |
-  //               std::views::filter(is_empty);
-  //
-  // fmt::print("{} \n", fmt::join(result, " "));
-  //
-  // ScratchCard card;
-  // for (auto const &token : result) {
-  //   // if (token.size() == 0 || token[0] == '|')
-  //   //   continue;
-  //
-  //   std::string tempo;
-  //   if (token.size() == 2) {
-  //     tempo.push_back(token[0]);
-  //     tempo.push_back(token[1]);
-  //   } else {
-  //     tempo.push_back(token[0]);
-  //   }
-  //
-  //   card.push_number(std::stoul(tempo));
-  // }
-
-  // card.show_res_debug();
 }
