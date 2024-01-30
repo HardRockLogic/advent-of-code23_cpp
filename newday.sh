882c3859352b73f02bd4691e3e2396eb97c0b8dc
HardRockLogic <neiton5001@gmail.com>
2024-01-30 16:24:16 +0200

    added clangd config file to newday template

--- newday.sh
+++ newday.sh
@@ -9,9 +9,10 @@ if [ $# -eq 1 ]; then
   mkdir $1
   touch $1/main.cpp $1/Makefile
   
+  # Creating default build
   cat <<'EOF' > $1/Makefile
 CXX = g++
-CXXFLAGS = -std=c++20 -Wall 
+CXXFLAGS = -std=c++20 -Wall -Wextra
 
 SRC = main.cpp
 EXE = main
@@ -23,6 +24,12 @@ $(EXE): $(SRC)
 
 clean: 
 	@rm -f $(EXE)    
+EOF
+
+  # Adding lsp support for c++20
+  cat <<-EOF > $1/.clangd
+CompileFlags:
+  Add: [-std=c++20]
 EOF
 
 else 
